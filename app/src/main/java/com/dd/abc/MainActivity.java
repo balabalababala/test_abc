f46cd4b5e005c4ca618c8eb8a59b004a381c649c
brad <2202089999@qq.com>
2023-09-09 22:48:43 +0800

    chore: singleTask

--- app/src/main/java/com/dd/abc/MainActivity.java
+++ app/src/main/java/com/dd/abc/MainActivity.java
@@ -2,9 +2,17 @@ package com.dd.abc;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.RadioGroup;
 
@@ -13,12 +21,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.dd.abc.databinding.ActivityMainBinding;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity implements Button.OnClickListener, RadioGroup.OnCheckedChangeListener {
     private MediaPlayer mediaPlayer = null;
     private boolean isReady = false;
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +66,102 @@ public class MainActivity extends AppCompatActivity implements Button.OnClickLis
             mediaPlayer.setLooping(true);
 //            mediaPlayer.start();
         }
+
+
+
+
+
+        WebView webView = findViewById(R.id.my_webview);
+
+        if (savedInstanceState != null && false) {
+
+            webView.restoreState(savedInstanceState);
+
+        } else {
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    String pattern = "^http";
+                    Pattern r = Pattern.compile(pattern);
+                    Matcher m = r.matcher(url);
+                    Log.w("url", "*****************" + url);
+                    if (m.find()) {
+                        view.loadUrl(url);
+                        return true;
+                    } else {
+                        return true; // 阻止打开
+                    }
+
+                }
+
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                    String url = request.getUrl().toString();
+
+                    Log.w("url", "*****************" + url);
+//                if (url.contains("example.com")) {
+//                    // Load the URL normally
+//                    return super.shouldInterceptRequest(view, request);
+//                } else {
+//                    // Block the URL
+//                    return new WebResourceResponse("text/plain", "UTF-8", null);
+//                }
+
+
+                    return super.shouldInterceptRequest(view, request);
+
+                }
+
+            });
+
+            // 播放视频要加载这个
+            webView.setWebChromeClient(new WebChromeClient());
+
+//        webView.
+
+
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            webView.getSettings().setJavaScriptEnabled(true); // 允许运行js
+            webView.getSettings().setLoadsImagesAutomatically(true);
+            webView.getSettings().setSupportMultipleWindows(true); // 可以加载多媒体，播放视频
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW); // 允许https页面加载http内容
+
+
+            webView.getSettings().setDomStorageEnabled(true);  // 启用localstorage
+//        webView.getSettings().setAppCacheMaxSize(1024*1024*8);
+//        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+//        webView.getSettings().setAppCachePath(appCachePath);
+            webView.getSettings().setAllowFileAccess(true);
+//        webView.getSettings().setAppCacheEnabled(true);
+
+            webView.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+            }
+
+            webView.loadUrl("https://www.baidu.com/");
+
+
+//        webView2 = new WebView(getActivity().getApplicationContext());
+//
+//        webView2.setWebViewClient(new WebViewClient(){
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+//        });
+//
+//        webView2.getSettings().setJavaScriptEnabled(true);
+//        webView2.loadUrl("https://www.alipay.com");
+//
+//        setContentView(webView2);
+
+        }
+
+
     }
 
 
--- app/src/main/java/com/dd/abc/ScrollingActivity.java
+++ app/src/main/java/com/dd/abc/ScrollingActivity.java
@@ -1,6 +1,7 @@
 package com.dd.abc;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -9,8 +10,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.dd.abc.databinding.ActivityScrollingBinding;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -23,24 +34,109 @@ public class ScrollingActivity extends AppCompatActivity {
         binding = ActivityScrollingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
+        View root = binding.getRoot();
 
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+
+
+        WebView webView = binding.myWebview;
+
+        if (savedInstanceState != null && false) {
+
+            webView.restoreState(savedInstanceState);
+
+        } else {
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    String pattern = "^http";
+                    Pattern r = Pattern.compile(pattern);
+                    Matcher m = r.matcher(url);
+                    Log.w("url", "*****************" + url);
+                    if (m.find()) {
+                        view.loadUrl(url);
+                        return true;
+                    } else {
+                        return true; // 阻止打开
+                    }
+
+                }
+
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                    String url = request.getUrl().toString();
+
+                    Log.w("url", "*****************" + url);
+//                if (url.contains("example.com")) {
+//                    // Load the URL normally
+//                    return super.shouldInterceptRequest(view, request);
+//                } else {
+//                    // Block the URL
+//                    return new WebResourceResponse("text/plain", "UTF-8", null);
+//                }
+
+
+                    return super.shouldInterceptRequest(view, request);
+
+                }
+
+            });
+
+            // 播放视频要加载这个
+            webView.setWebChromeClient(new WebChromeClient());
+
+//        webView.
+
+
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+            webView.getSettings().setJavaScriptEnabled(true); // 允许运行js
+            webView.getSettings().setLoadsImagesAutomatically(true);
+            webView.getSettings().setSupportMultipleWindows(true); // 可以加载多媒体，播放视频
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW); // 允许https页面加载http内容
+
+
+            webView.getSettings().setDomStorageEnabled(true);  // 启用localstorage
+//        webView.getSettings().setAppCacheMaxSize(1024*1024*8);
+//        String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
+//        webView.getSettings().setAppCachePath(appCachePath);
+            webView.getSettings().setAllowFileAccess(true);
+//        webView.getSettings().setAppCacheEnabled(true);
+
+            webView.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
             }
-        });
+
+            webView.loadUrl("https://www.baidu.com/");
 
 
-        // 获取主Activity传递的数据
+//        webView2 = new WebView(getActivity().getApplicationContext());
+//
+//        webView2.setWebViewClient(new WebViewClient(){
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                view.loadUrl(url);
+//                return true;
+//            }
+//        });
+//
+//        webView2.getSettings().setJavaScriptEnabled(true);
+//        webView2.loadUrl("https://www.alipay.com");
+//
+//        setContentView(webView2);
+
+        }
         Intent intent = getIntent();
         String string = intent.getStringExtra("extra_data");
         Log.w("*******",string);
+    }
+    public void startMain(View v){
+        Intent intent = new Intent();
+        intent.setClassName("com.dd.abc","com.dd.abc.MainActivity");
+        intent.putExtra("extra_data", "Hello World");
+        startActivity(intent);
     }
 }
